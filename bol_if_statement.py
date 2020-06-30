93edde2fa6ff1f56adf79d22eeb39af1cf58bdff
varunpreet-code <cp0452472@gmail.com>
2020-06-30 22:31:27 +0800

    new

---
+++ bol_if_statement.py
@@ -0,0 +1,23 @@
+
+moths_in_house= True
+if moths_in_house:
+    print("Get the moths!")
+else:  
+  print("No threats detected.")
+  
+#
+moths_in_house = False
+if moths_in_house:
+    print("Get the moths!")
+else:  
+  print("No threats detected.")
+  #
+  moths_in_house= True
+  mitch_is_home= True
+ if moths_in_house:
+   print("Hooman! Help me get the moths !")
+if condition:
+  pass
+else:
+  pass mitch_is_home:
+   print("Hooman! Help me get the moths !")
---
+++ var_user.py
@@ -0,0 +1,48 @@
+x = 3
+y = 9
+z = x + y
+print (z)
+
+#
+x = -3
+y = 9
+z = x + y
+print (z)
+
+#
+x = 3.0
+y = -9
+z = x + y
+print (z)
+#
+x = 3
+y = 9
+z = x * y
+print (z)
+
+#
+x = 3.0
+y = -9
+z = x * y
+print (y)
+#
+
+x = " William is "
+y = " 192 cms talls"
+z =  x + y
+print(z)
+#
+x = " 192 cms talls "
+
+def myfunc():
+    print ( "William is " + x)
+myfunc()
+#
+x = " 27 cms tall "
+
+def myfunc():
+    print ( "Roary is " + x)
+myfunc()
+# kilo
+kilometers = float(input("10 km: "))
+#
