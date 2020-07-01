502238da8633ed07c29a9ea0316dc401c0595fe7
varunpreet-code <cp0452472@gmail.com>
2020-07-01 17:31:28 +0800

    new

--- bol_if_statement.py
+++ bol_if_statement.py
@@ -11,13 +11,76 @@ if moths_in_house:
     print("Get the moths!")
 else:  
   print("No threats detected.")
-  #
-  moths_in_house= True
-  mitch_is_home= True
- if moths_in_house:
-   print("Hooman! Help me get the moths !")
-if condition:
-  pass
+  # if statement 
+  moths_in_house = True
+  mitch_is_home = True
+  if moths_in_house and mitch_is_home:
+    print("Hooman! Help me get the moths!")
+  
+  moths_in_house = True
+mitch_is_home = False
+
+if moths_in_house and mitch_is_home:
+    print("Hooman! Help me get the moths!")
+elif not moths_in_house and not mitch_is_home:
+    print("No threats detected.")
+elif moths_in_house and not mitch_is_home:
+    print("Meooooooooooooow! Hissssss!")
 else:
-  pass mitch_is_home:
-   print("Hooman! Help me get the moths !")
+    print("Climb on Mitch.")
+#
+    moths_in_house = False
+mitch_is_home = False
+if moths_in_house and mitch_is_home:
+    print("Hooman! Help me get the moths!")
+elif not moths_in_house and not mitch_is_home:
+    print("No threats detected.")
+elif moths_in_house and not mitch_is_home:
+    print("Meooooooooooooow! Hissssss!")
+else:
+    print("Climb on Mitch.")
+  #
+  
+    moths_in_house = True
+mitch_is_home = False
+if moths_in_house and mitch_is_home:
+    print("Hooman! Help me get the moths!")
+elif not moths_in_house and not mitch_is_home:
+    print("No threats detected.")
+elif moths_in_house and not mitch_is_home:
+    print("Meooooooooooooow! Hissssss!")
+else:
+    print("Climb on Mitch.")
+    #
+moths_in_house = True
+mitch_is_home = True
+if moths_in_house and mitch_is_home:
+    print("Hooman! Help me get the moths!")
+elif not moths_in_house and not mitch_is_home:
+    print("No threats detected.")
+elif moths_in_house and not mitch_is_home:
+    print("Meooooooooooooow! Hissssss!")
+else:
+    print("Climb on Mitch.")
+
+###########
+light_colour = "Red"
+car_detected = False
+if light_colour == "Red" and not car_detected:
+    print("Do nothing.")
+elif light_colour == "Red" and car_detected:
+    print("Flash")
+elif light_colour == "Green" and not car_detected:
+    print("Do Nothing")
+elif light_colour == "Green" and car_detected:
+    print("Do nothing")
+elif light_colour == "Amber" and not car_detected:
+    print("Do nothing")
+elif light_colour == "Amber" and car_detected:
+    print("Do nothing")
+else:
+    print("Do nothing")
+      
+    
+
+    
--- conditionals/booleans.py
+++ conditionals/booleans.py
@@ -26,3 +26,19 @@ is_raining = True
 is_cold = True
 print(is_raining or is_cold, is_raining or not is_cold)
 
+
+    
+light_colour = "Red"
+    car_detected = False
+    if light_colour == "Red" and not car_detected:
+      print("Do nothing.")
+    elif light_colour == "Red" and  car_detected:
+      print("Flash")
+    elif light_colour == "Green" and not car_detected:
+      print("Do Nothing")
+    elif light_colour == "Green" and  car_detected:
+      print("Do nothing")
+    elif light_colour == "Amber" and not car_detected:
+      print("Do nothing")
+    else light_colour == "Amber" and car_detected:
+      print("Do nothing")
